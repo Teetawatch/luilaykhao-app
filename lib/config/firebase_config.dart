18ddec44abc7d7de30d8635ce4d55ba9c3796d6b
Teetawatch Phiphatdetthana <teetawatch.p@gmail.com>
2026-05-15 11:14:53 +0700

    Add iOS app support

--- lib/config/firebase_config.dart
+++ lib/config/firebase_config.dart
@@ -1,16 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 class FirebaseConfig {
   static const projectId = String.fromEnvironment('FIREBASE_PROJECT_ID');
   static const androidAppId = String.fromEnvironment('FIREBASE_ANDROID_APP_ID');
+  static const iosAppId = String.fromEnvironment('FIREBASE_IOS_APP_ID');
+  static const iosBundleId = String.fromEnvironment(
+    'FIREBASE_IOS_BUNDLE_ID',
+    defaultValue: 'com.luilaykhao.app',
+  );
   static const apiKey = String.fromEnvironment('FIREBASE_API_KEY');
   static const messagingSenderId = String.fromEnvironment(
     'FIREBASE_MESSAGING_SENDER_ID',
   );
 
+  static String get appId {
+    return defaultTargetPlatform == TargetPlatform.iOS ? iosAppId : androidAppId;
+  }
+
   static bool get hasDartDefines {
     return projectId.isNotEmpty &&
-        androidAppId.isNotEmpty &&
+        appId.isNotEmpty &&
         apiKey.isNotEmpty &&
         messagingSenderId.isNotEmpty;
   }
@@ -18,11 +28,12 @@ class FirebaseConfig {
   static FirebaseOptions? get options {
     if (!hasDartDefines) return null;
 
-    return const FirebaseOptions(
+    return FirebaseOptions(
       apiKey: apiKey,
-      appId: androidAppId,
+      appId: appId,
       messagingSenderId: messagingSenderId,
       projectId: projectId,
+      iosBundleId: iosBundleId,
     );
   }
 }
